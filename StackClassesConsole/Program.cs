355f809fda990822a38238c17d2a0026e3d007e7
salaga999999999 <salaga999999999@gmail.com>
2025-03-04 22:00:08 +0700

    Main task complite

--- StackClassesConsole/Program.cs
+++ StackClassesConsole/Program.cs
@@ -4,6 +4,23 @@
     {
         static void Main(string[] args)
         {
+            Stack stack = new Stack("a", "b");
+            stack.Add(null);
+            stack.Add("c");
+
+            Console.WriteLine("Top: " + stack.Top());
+
+            Console.WriteLine(stack.Put() + " Size = " + stack.Size);
+            Console.WriteLine(stack.Put() + " Size = " + stack.Size);
+            Console.WriteLine(stack.Put() + " Size = " + stack.Size);
+            //Console.WriteLine(stack.Put() + " Size = " + stack.Size);
+
+            Console.WriteLine("Top: " + (stack.Top()==null? "null" :"not a null" ));
+
+
+
+            stack.ShowAllElements();
+
             Console.WriteLine("Hello, World!");
         }
     }
---
+++ StackClassesConsole/Stack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackClassesConsole
+{
+    public class Stack
+    {
+        public List<string> stack;
+        public int Size => stack.Count;
+        public Stack(params string[] items)
+        {
+            stack = new List<string>(items);
+        }
+        public bool Add(string item)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(item))
+                {
+                    stack.Add(item);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+        public string Put()
+        {
+            if (stack.Count == 0 || stack == null)
+            {
+                throw new Exception("Стек пустой");
+            }
+            try
+            {
+                string tempString = stack.Last();
+                stack.RemoveAt(stack.Count - 1);
+                return tempString;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        public string Top()
+        {
+            if(stack.Count == 0 || stack == null)
+            {
+                return null;
+            }
+            else
+            {
+                return stack.Last();
+            }
+        }
+        public void ShowAllElements()
+        {
+            int count = 0;
+            foreach (var item in stack)
+            {
+                Console.WriteLine(count + ": " + item);
+                count++;
+            }
+        }
+    }
+}
