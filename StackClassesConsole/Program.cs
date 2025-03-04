03aa58a8bca38dde8c1f60cbec04425b3342a255
salaga999999999 <salaga999999999@gmail.com>
2025-03-04 21:29:55 +0700

    Solution created

---
+++ StackClassesConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackClassesConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
